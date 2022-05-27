518d30ea41ca0c4f99dd67b8270c97a42a90806b
dania-spb@mail.ru <dania199714101961>
2022-05-27 19:16:28 +0300

    lesson 7, task 3 - third page

--- lib/lesson7/UI/pages/third_page.dart
+++ lib/lesson7/UI/pages/third_page.dart
@@ -15,8 +15,52 @@ class ThirdPage extends StatelessWidget {
   }
 
   buildBody(BuildContext context) {
-    return const Center(
-      child: Text(title),
+    return Column(
+      children: [
+        const Text(title),
+        Divider(),
+        Expanded(
+          child: Container(
+            alignment: Alignment.center,
+            child: SizedBox(
+              width: 200,
+              height: 200,
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.green.shade100,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Stack(children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                          width: 20,
+                          height: 20,
+                          decoration: const BoxDecoration(
+                            color: Colors.red,
+                          )),
+                      Container(
+                          width: 20,
+                          height: 20,
+                          decoration: const BoxDecoration(
+                            color: Colors.green,
+                          )),
+                      Container(
+                          width: 20,
+                          height: 20,
+                          decoration: const BoxDecoration(
+                            color: Colors.blue,
+                          )),
+                    ],
+                  ),
+                  const Divider(),
+                ]),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
