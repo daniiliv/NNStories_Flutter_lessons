7b1834b6b46287d5a64f59ea7224efea18e72b09
dania-spb@mail.ru <dania-spb@mail.ru>
2022-06-03 15:06:36 +0300

    Function for checking a word in Oxford dictionary + unit tests for it.

---
+++ lib/helping/oxford_check.dart
@@ -0,0 +1,30 @@
+import 'package:http/http.dart' as http;
+
+Future<bool> isWordInOxfordDictionary(String word) async {
+  bool result = false;
+
+  const String appId = "51476dde";
+  const String appKey = "d130aa3cbe7fb48cbaf8d9494126a679";
+
+  const String language = "en";
+  final String wordId = word.toLowerCase();
+
+  final String uri =
+      "https://od-api.oxforddictionaries.com:443/api/v2/lemmas/$language/$wordId";
+
+  var response = await http.get(
+    Uri.parse(uri),
+    headers: <String, String>{
+      'app_id': appId,
+      'app_key': appKey,
+    },
+  );
+
+  // Если код состояния 200, значит слово было найдено в словаре.
+  // В ином случае возвращается код 404, то есть слово не найдено.
+  if (response.statusCode == 200) {
+    result = true;
+  }
+
+  return result;
+}
---
+++ test/oxford_check_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nnstories_lessons/helping/oxford_check.dart';
+
+void main() {
+  group('Counter', () {
+    test('"harassment" should be found in dictionary', () async {
+      bool result = await isWordInOxfordDictionary("harassment");
+
+      expect(result, true);
+    });
+
+    test('"some text" shouldn\'t be found in dictionary', () async {
+      bool result = await isWordInOxfordDictionary("some text");
+
+      expect(result, false);
+    });
+
+    test('empty string shouldn\'t be found in dictionary', () async {
+      bool result = await isWordInOxfordDictionary("");
+
+      expect(result, false);
+    });
+
+    test('"asdasdasdasd" should be found in dictionary', () async {
+      bool result = await isWordInOxfordDictionary("asdasdasdasd");
+
+      expect(result, false);
+    });
+
+    test('digits shouldn\'t be found in dictionary', () async {
+      bool result = await isWordInOxfordDictionary("123");
+
+      expect(result, false);
+    });
+  });
+}
